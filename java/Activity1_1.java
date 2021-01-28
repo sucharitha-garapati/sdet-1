cc1f429c3904c82e791ebf067f57a02f646e8a42
sucharitha-garapati <78101503+sucharitha-garapati@users.noreply.github.com>
2021-01-28 21:45:55 +0530

    Add files via upload

--- java/Activity1_1.java
+++ java/Activity1_1.java
@@ -1,1 +1,17 @@
+package javaActivity1;
 
+public class Activity1_1 {
+
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "Black";
+        toyota.transmission = "Manual";
+        
+        //call methods
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+    }
+
+}
