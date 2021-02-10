53c445a0b6ef55badf89e486a792bf709e6201ab
sucharitha-garapati <78101503+sucharitha-garapati@users.noreply.github.com>
2021-02-10 09:32:14 +0530

    Add files via upload

---
+++ selenium/Activity1.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+    public static void main(String args[]) throws InterruptedException {
+    	
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net");
+        Thread.sleep(1000);
+        driver.close();
+    }
+}
